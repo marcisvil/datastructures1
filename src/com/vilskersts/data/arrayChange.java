2ecc0c9cd935364925a8da5832d4fbbf6203a87a
marcisvil <44918188+marcisvil@users.noreply.github.com>
2018-11-25 11:48:48 +0200

    Merge branch 'third'
    # Conflicts:
    #	src/com/vilskersts/data/arrayChange.java

--- src/com/vilskersts/data/arrayChange.java
+++ src/com/vilskersts/data/arrayChange.java
@@ -7,7 +7,44 @@ public class ArrayChange {
     String o;
     String p;
 
+    public String getA() {
+        return a;
+    }
 
+    public void setA(String a) {
+        this.a = a;
+    }
 
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getO() {
+        return o;
+    }
+
+    public void setO(String o) {
+        this.o = o;
+    }
+
+    public String getP() {
+        return p;
+    }
+
+    public void setP(String p) {
+        this.p = p;
+    }
 }
 
