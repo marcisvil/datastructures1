23367afb06d5a04cba5c9f9102913abeed4d652e
marcisvil <44918188+marcisvil@users.noreply.github.com>
2018-11-25 11:42:59 +0200

    Mans otrais commits

--- src/com/vilskersts/data/arrayChange.java
+++ src/com/vilskersts/data/arrayChange.java
@@ -6,5 +6,45 @@ public class ArrayChange {
     String c;
     String v;
     String n;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getV() {
+        return v;
+    }
+
+    public void setV(String v) {
+        this.v = v;
+    }
+
+    public String getN() {
+        return n;
+    }
+
+    public void setN(String n) {
+        this.n = n;
+    }
 }
 
